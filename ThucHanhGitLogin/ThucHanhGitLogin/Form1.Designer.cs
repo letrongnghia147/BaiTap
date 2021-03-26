3967debcc67dc2bbffa5787d18c7ec53828aa74a
nghia le <letrongnghia147@gmail.com>
2021-03-26 08:32:34 +0700

    btnLogin

--- ThucHanhGitLogin/ThucHanhGitLogin/Form1.Designer.cs
+++ ThucHanhGitLogin/ThucHanhGitLogin/Form1.Designer.cs
@@ -34,74 +34,78 @@
             this.label3 = new System.Windows.Forms.Label();
             this.lblPass = new System.Windows.Forms.Label();
             this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtUser
             // 
-            this.txtUser.Location = new System.Drawing.Point(282, 172);
-            this.txtUser.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtUser.Location = new System.Drawing.Point(141, 89);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(844, 38);
+            this.txtUser.Size = new System.Drawing.Size(424, 22);
             this.txtUser.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(200, 45);
+            this.label1.Size = new System.Drawing.Size(100, 23);
             this.label1.TabIndex = 0;
             // 
             // lblUser
             // 
             this.lblUser.AutoSize = true;
-            this.lblUser.Location = new System.Drawing.Point(50, 182);
-            this.lblUser.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblUser.Location = new System.Drawing.Point(25, 94);
             this.lblUser.Name = "lblUser";
-            this.lblUser.Size = new System.Drawing.Size(149, 32);
+            this.lblUser.Size = new System.Drawing.Size(75, 17);
             this.lblUser.TabIndex = 1;
             this.lblUser.Text = "UserName";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(536, 60);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Location = new System.Drawing.Point(268, 31);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(112, 32);
+            this.label3.Size = new System.Drawing.Size(56, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "BaiTest";
             // 
             // lblPass
             // 
             this.lblPass.AutoSize = true;
-            this.lblPass.Location = new System.Drawing.Point(50, 301);
-            this.lblPass.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblPass.Location = new System.Drawing.Point(25, 155);
             this.lblPass.Name = "lblPass";
-            this.lblPass.Size = new System.Drawing.Size(78, 32);
+            this.lblPass.Size = new System.Drawing.Size(39, 17);
             this.lblPass.TabIndex = 4;
             this.lblPass.Text = "Pass";
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(282, 291);
-            this.txtPass.Margin = new System.Windows.Forms.Padding(6);
+            this.txtPass.Location = new System.Drawing.Point(141, 150);
             this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(844, 38);
+            this.txtPass.Size = new System.Drawing.Size(424, 22);
             this.txtPass.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(490, 203);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1236, 872);
+            this.ClientSize = new System.Drawing.Size(587, 256);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.lblPass);
             this.Controls.Add(this.txtPass);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lblUser);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtUser);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -118,6 +122,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblPass;
         private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
